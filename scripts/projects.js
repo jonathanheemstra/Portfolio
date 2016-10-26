40d6c5ed3dce4d299afe461467985b9addbdc9f4
Jonathan Heemstra <jonathan.heemstra@gmail.com>
2016-10-26 09:47:31 -0700

    built projects constructor function - doesn't include prototypes

---
+++ scripts/projects.js
@@ -0,0 +1,9 @@
+var projects = [];
+
+function Projects (project){
+  this.title = project.title;
+  this.img = project.img;
+  this.projectUrl = project.projectUrl;
+  this.body = project.body;
+  this.skills = project.skills;
+}
