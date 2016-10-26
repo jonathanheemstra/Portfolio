e121e49706b4805d2294bda0e4b4296f9dc23e49
Jonathan Heemstra <jonathan.heemstra@gmail.com>
2016-10-26 09:38:18 -0700

    add scripts folder, projects.js, projectsList.js, populate projectsList.js with data

---
+++ scripts/projectsList.js
@@ -0,0 +1,23 @@
+var projects = [
+  {
+    title:      'Project 1';
+    img:        'http://placehold.it/350x150';
+    projectUrl: 'https://www.github.com/jonathanheemstra';
+    body:       '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>';
+    skills:     ['skill 1', 'skill 2', 'skill 3'];
+  },
+  {
+    title:      'Project 2';
+    img:        'http://placehold.it/350x150';
+    projectUrl: 'https://www.github.com/jonathanheemstra';
+    body:       '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>';
+    skills:     ['skill 1', 'skill 2', 'skill 3'];
+  },
+  {
+    title:      'Project 3';
+    img:        'http://placehold.it/350x150';
+    projectUrl: 'https://www.github.com/jonathanheemstra';
+    body:       '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>';
+    skills:     ['skill 1', 'skill 2', 'skill 3'];
+  }
+];
