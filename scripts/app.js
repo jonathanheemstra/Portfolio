266ed195b0c7f94c57717c386a1320a2fb27e3e0
Jonathan Heemstra <jonathan.heemstra@gmail.com>
2016-10-27 10:40:22 -0700

    add expand and collapse to project description

--- scripts/app.js
+++ scripts/app.js
@@ -11,7 +11,20 @@ portfolioView.handleNav = function () {
 
 portfolioView.learnMoreExpand = function () {
   $('.project_description *:nth-of-type(n+2)').hide();
+  $('#projects').on('click', '.learn_more', function(event){
+    event.preventDefault();
+    $(this).prev().children('.project_description *:nth-of-type(n+2)').fadeIn();
+    $(this).html('Collapse  &rarr;').removeClass('learn_more').addClass('learn_more_collapse');
+  });
+};
+portfolioView.learnMoreCollapse = function () {
+  $('#projects').on('click', '.learn_more_collapse', function(event) {
+    event.preventDefault();
+    $(this).prev().children('.project_description *:nth-of-type(n+2)').fadeOut();
+    $(this).html('Learn more  &rarr;').removeClass('learn_more_collapse').addClass('learn_more');
+  });
 };
 
 portfolioView.handleNav();
 portfolioView.learnMoreExpand();
+portfolioView.learnMoreCollapse();
