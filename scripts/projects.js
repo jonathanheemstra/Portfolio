7b1e1dc644abd31f3bbe812c351cb70d56dfb401
Jonathan Heemstra <jonathan.heemstra@gmail.com>
2016-10-28 09:32:57 -0700

    add project template via handlebars

--- scripts/projects.js
+++ scripts/projects.js
@@ -9,15 +9,18 @@ function Projects (project){
   this.date = project.projectDate;
 }
 Projects.prototype.toHtml = function () {
-  var $newProject = $('article.project_template').clone().removeClass('project_template').addClass('project');
-  $newProject.find('h4 a:contains("Title")').text(this.title);
-  $newProject.find('img').attr('src', this.img);
-  $newProject.find('a').attr('href', this.projectUrl);
-  $newProject.find('.project_description').html(this.body);
-  $newProject.find('.project_skills').html(this.skills);
-  $newProject.find('time[pubdate]').attr('datetime', this.date);
-  $newProject.find('time').text(this.date);
-  return $newProject;
+  var source = $('#project_template').html();
+  var templateRender = Handlebars.compile(source);
+  return templateRender(this);
+  // var $newProject = $('article.project_template').clone().removeClass('project_template').addClass('project');
+  // $newProject.find('h4 a:contains("Title")').text(this.title);
+  // $newProject.find('img').attr('src', this.img);
+  // $newProject.find('a').attr('href', this.projectUrl);
+  // $newProject.find('.project_description').html(this.body);
+  // $newProject.find('.project_skills').html(this.skills);
+  // $newProject.find('time[pubdate]').attr('datetime', this.date);
+  // $newProject.find('time').text(this.date);
+  // return $newProject;
 };
 projectsList.sort(function(currentProject, nextProject) {
   return (new Date(nextProject.projectDate)) - (new Date(currentProject.projectDate));
