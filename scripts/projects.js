24aed5833baee11f114ffd3889e21c75194ab250
Jonathan Heemstra <jonathan.heemstra@gmail.com>
2016-10-28 11:51:08 -0700

    add handlebars helpers to show skills, some style updates, main nav needs better styles

--- scripts/projects.js
+++ scripts/projects.js
@@ -12,21 +12,19 @@ Projects.prototype.toHtml = function () {
   var source = $('#project_template').html();
   var templateRender = Handlebars.compile(source);
   return templateRender(this);
-  // var $newProject = $('article.project_template').clone().removeClass('project_template').addClass('project');
-  // $newProject.find('h4 a:contains("Title")').text(this.title);
-  // $newProject.find('img').attr('src', this.img);
-  // $newProject.find('a').attr('href', this.projectUrl);
-  // $newProject.find('.project_description').html(this.body);
-  // $newProject.find('.project_skills').html(this.skills);
-  // $newProject.find('time[pubdate]').attr('datetime', this.date);
-  // $newProject.find('time').text(this.date);
-  // return $newProject;
 };
 projectsList.sort(function(currentProject, nextProject) {
   return (new Date(nextProject.projectDate)) - (new Date(currentProject.projectDate));
 });
 projectsList.forEach(function(project) {
   projects.push(new Projects(project));
+});
+Handlebars.registerHelper('list', function (skills, options) {
+  var ulEl = '<ul class="skills">';
+  for (var i = 0, j = skills.length; i < j; i++) {
+    ulEl = ulEl + '<li>' + options.fn(skills[i]) + '</li>';
+  }
+  return ulEl + '</ul>';
 });
 projects.forEach(function(project){
   $('#projects').append(project.toHtml());
