ac76b350f70b8f9e182448cc59e2a29a98d54b4d
Jonathan Heemstra <jonathan.heemstra@gmail.com>
2016-10-31 14:57:07 -0700

    fix nav

--- scripts/projects.js
+++ scripts/projects.js
@@ -1,15 +1,5 @@
 var projects = [];
-
-// function Projects (project){
-//   this.title = project.title;
-//   this.img = project.img;
-//   this.projectUrl = project.projectUrl;
-//   this.body = project.body;
-//   this.skills = project.skills;
-//   this.date = project.projectDate;
-// }
-
-//Object constructor function
+// Object constructor function
 /*
   the data we are getting is coming from the function:
 
@@ -17,7 +7,7 @@ var projects = [];
         projects.push(new Projects(project));
       });
 
-  for each is looping through     
+  for each is looping through
   Key = the key in the projectsList object
 */
 function Projects (projectsList) {
@@ -26,14 +16,17 @@ function Projects (projectsList) {
   }
 }
 
+// Function to get the project template from index.html and then compile and render the template.
 Projects.prototype.toHtml = function () {
   var source = $('#project_template').html();
   var templateRender = Handlebars.compile(source);
   return templateRender(this);
 };
+// Function to sort the projectsList array of objects by date.
 projectsList.sort(function(currentProject, nextProject) {
   return (new Date(nextProject.projectDate)) - (new Date(currentProject.projectDate));
 });
+// Push each constrcutred object from the projectsList into the projects array.
 projectsList.forEach(function(project) {
   projects.push(new Projects(project));
 });
