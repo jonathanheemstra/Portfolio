39b54e6fa78badde6945b5be04b69799d11b8dd6
Jonathan Heemstra <jonathan.heemstra@gmail.com>
2016-11-01 11:11:23 -0700

    render projects list from json database

--- scripts/projects.js
+++ scripts/projects.js
@@ -1,4 +1,3 @@
-var projects = [];
 // Object constructor function
 /*
   the data we are getting is coming from the function:
@@ -16,20 +15,46 @@ function Projects (projectsList) {
   }
 }
 
+Projects.allProjects = [];
+
 // Function to get the project template from index.html and then compile and render the template.
 Projects.prototype.toHtml = function () {
   var source = $('#project_template').html();
   var templateRender = Handlebars.compile(source);
   return templateRender(this);
 };
-// Function to sort the projectsList array of objects by date.
-projectsList.sort(function(currentProject, nextProject) {
-  return (new Date(nextProject.projectDate)) - (new Date(currentProject.projectDate));
-});
-// Push each constrcutred object from the projectsList into the projects array.
-projectsList.forEach(function(project) {
-  projects.push(new Projects(project));
-});
+
+Projects.loadAll = function (inputProjects) {
+  // Function to sort the projectsList array of objects by date.
+  inputProjects.sort(function (currentProject, nextProject) {
+    return (new Date(nextProject.projectDate)) - (new Date(currentProject.projectDate));
+  })
+  .forEach(function(ele) {
+    // Push each constrcutred object from the projectsList into the projects array.
+    Projects.allProjects.push(new Projects(ele));
+  });
+};
+
+Projects.fetchAll = function () {
+  if (localStorage.projects) {
+    var projects = JSON.parse(localStorage.getItem('projects'));
+    Projects.loadAll(projects);
+    Projects.allProjects.forEach(function(project){
+      $('#projects').append(project.toHtml());
+    });
+    console.log('Loaded from Local');
+  } else {
+    $.getJSON('../data/projectsData.json', function (projects) {
+      Projects.loadAll(projects);
+      localStorage.setItem('projects', JSON.stringify(projects));
+      Projects.allProjects.forEach(function(project){
+        $('#projects').append(project.toHtml());
+      });
+      console.log('Loaded from database');
+    });
+  }
+};
+
 Handlebars.registerHelper('list', function (skills, options) {
   var ulEl = '<ul class="skills">';
   for (var i = 0, j = skills.length; i < j; i++) {
@@ -37,6 +62,5 @@ Handlebars.registerHelper('list', function (skills, options) {
   }
   return ulEl + '</ul>';
 });
-projects.forEach(function(project){
-  $('#projects').append(project.toHtml());
-});
+
+Projects.fetchAll();
