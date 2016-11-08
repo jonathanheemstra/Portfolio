4beec2298b500b6ff9ee302dd34eadda80a64cb0
Jonathan Heemstra <jonathan.heemstra@gmail.com>
2016-11-08 12:35:05 -0800

    fix bug on show more/show less expander for project description

--- scripts/views/app.js
+++ scripts/views/app.js
@@ -4,23 +4,21 @@
   var portfolioView = {};
   // Expand and Collapse project descriptions
   portfolioView.learnMoreExpand = function () {
-    $('.project_description *:nth-of-type(n+2)').hide();
-    $('#projects').on('click', '.learn_more', function(event){
+    $('.project_body *:nth-of-type(n+2)').hide();
+    $('.project').on('click', '.learn_more', function(event){
       event.preventDefault();
-      $(this).prev().children('.project_description *:nth-of-type(n+2)').slideDown();
-      $(this).html('Collapse  &rarr;').removeClass('learn_more').addClass('learn_more_collapse');
-    });
-  };
-  
-  portfolioView.learnMoreCollapse = function () {
-    $('#projects').on('click', '.learn_more_collapse', function(event) {
-      event.preventDefault();
-      $(this).prev().children('.project_description *:nth-of-type(n+2)').slideUp();
-      $(this).html('Learn more  &rarr;').removeClass('learn_more_collapse').addClass('learn_more');
+      $(this).parent().prev().find('.project_body *:nth-of-type(n+2)').slideDown();
+      $(this).html('Show Less  &rarr;').removeClass('learn_more').addClass('learn_more_collapse');
     });
   };
 
-  // Invoke all functions
+  portfolioView.learnMoreCollapse = function () {
+    $('.project').on('click', '.learn_more_collapse', function(event) {
+      event.preventDefault();
+      $(this).parent().prev().find('.project_body *:nth-of-type(n+2)').slideUp();
+      $(this).html('Show More  &rarr;').removeClass('learn_more_collapse').addClass('learn_more');
+    });
+  };
 
   module.portfolioView = portfolioView;
 })(window);
