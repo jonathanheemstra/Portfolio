4b5d254534f90cbf4a81c23052843f3ee414d585
Jonathan Heemstra <jonathan.heemstra@gmail.com>
2016-11-09 12:14:29 -0800

    render github stats to portfolio

--- scripts/models/repo.js
+++ scripts/models/repo.js
@@ -3,14 +3,22 @@
   var repos = {};
 
   repos.allRepos = [];
-  repos.getRepos = function () {
+
+  repos.getRepos = function (callback) {
     $.ajax({
       url: 'https://api.github.com/users/jonathanheemstra/repos',
       type: 'GET',
       header: {'Authorization': 'token ' + githubToken},
       success: function(data){
         repos.allRepos = data;
+        callback();
       }
+    });
+  };
+
+  repos.githubAttr = function(attr) {
+    return repos.allRepos.filter(function(aRepo) {
+      return aRepo[attr];
     });
   };
 
